c3d6805a69f88d0e8986efa14fde5ae797e73dca
Raaj Kanchan <iamraajkanchan@gmail.com>
2018-09-06 10:20:03 +0530

    ImageView Added
    ImageView Added in Database Demo. Awesome,

--- 2018/August/18/DatabaseDemo/app/src/main/java/databasedemo/com/example/raj/ivy/databasedemo/UserProfile.java
+++ 2018/August/18/DatabaseDemo/app/src/main/java/databasedemo/com/example/raj/ivy/databasedemo/UserProfile.java
@@ -1,10 +1,14 @@
 package databasedemo.com.example.raj.ivy.databasedemo;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import databasedemo.com.example.raj.ivy.model.UserData;
 
@@ -14,6 +18,10 @@ public class UserProfile extends AppCompatActivity {
     EditText userProfile_Name;
     EditText userProfile_Email;
     EditText userProfile_Password;
+    ImageView userProfile_Image;
+
+    private static final int PICK_IMAGE = 100;
+    Uri imageURI;
 
     DBAdapter dbAdapter;
 
@@ -26,6 +34,7 @@ public class UserProfile extends AppCompatActivity {
         userProfile_Name = findViewById( R.id.userProfile_Name);
         userProfile_Email = findViewById( R.id.userProfile_Email);
         userProfile_Password = findViewById( R.id.userProfile_Password);
+        userProfile_Image = findViewById(R.id.userProfile_Image);
 
         dbAdapter = new DBAdapter(UserProfile.this);
 
@@ -55,6 +64,24 @@ public class UserProfile extends AppCompatActivity {
         String deleteusername = userProfile_Username.getText().toString();
         dbAdapter.deleteUser(deleteusername);
 
+    }
+
+    public void userProfile_Image_Clicked(View view){
+        openGallery();
+    }
+
+    public void openGallery(){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageURI = data.getData();
+            userProfile_Image.setImageURI(imageURI);
+        }
     }
 
 }
